e1f3bca5f26e78afac933518dafeff8b2ffdb38d
Yaduo Liu <yaduol@hotmail.com>
2021-03-31 16:24:38 -0700

    ch3

---
+++ ch3/3-2/LanzhouBeefNoodles/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LanzhouBeefNoodles.Controllers
+{
+    [Route("")]
+    //[Route("home")]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        [Route("")]
+        //[Route("index")]
+        [Route("[action]")]
+        public String Index()
+        {
+            return "Hello from home";
+        }
+
+        //[Route("about")]
+        [Route("[action]")]
+        public String About()
+        {
+            return "Hello from About";
+        }
+    }
+}
